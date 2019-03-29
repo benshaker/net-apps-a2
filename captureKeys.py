c12bbd78bfcc806f826d42aa50b13a1bb12a21da
Stevent7 <stevent7@vt.edu>
2019-03-28 23:02:16 -0400

    Added AccessKeys

--- captureKeys.py
+++ captureKeys.py
@@ -4,3 +4,5 @@
 # Key & Secret for the Twitter Streaming API
 APIKey = "GP6uGCcBf6bSV1Dh1cb1dTRRy"
 APISecretKey = "66k1Z9ntURfRhtui3q8wMQYHcv7HJb2GS78StEbnznuhcrUxDx"
+AccessKey = "1111413862207754240-zQdyKSjxXkAPHDZLnswvFOWybXmi0l"
+AccessSecretKey = "bbLOgAEKY17fQIIAsBbx3LrVGbxUj4Lxb3QeFiUeuyuol"
