7bfa8b9985ce42bc0fdf9b71a4f7c721a63071f2
Stevent7 <stevent7@vt.edu>
2019-03-29 23:55:17 -0400

    Added Tweet Parsing

--- capture.py
+++ capture.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # capture.py
 
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 import argparse
 import os
@@ -12,15 +12,77 @@ from tweepy import Stream
 from tweepy.streaming import StreamListener
 import pika, sys, tweepy
 
+# setup pins
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT) # Red
+GPIO.setup(13, GPIO.OUT) # Green
+GPIO.setup(15, GPIO.OUT) # Blue
+
+def setLEDW(self):
+        # white LED - Waiting for command
+	GPIO.output(11, GPIO.HIGH)
+	GPIO.output(13, GPIO.HIGH)
+	GPIO.output(15, GPIO.HIGH)
+	time.sleep(3)
+
+def setLEDR(self):
+        # red LED - Received pulish request
+        GPIO.output(11, GPIO.HIGH)
+	GPIO.output(13, GPIO.LOW)
+	GPIO.output(15, GPIO.LOW)
+	time.sleep(3)
+
+def setLEDG(self):
+	# green LED - Received consume request
+	GPIO.output(11, GPIO.LOW)
+	GPIO.output(13, GPIO.HIGH)
+	GPIO.output(15, GPIO.LOW)
+	time.sleep(3)
+
 class listener(StreamListener):
 
+        def setIP(self, ip):
+                self.ip = ip
+
 	def on_data(self, data):
-		tweet = all_data["text"]
-		tweet = tweet.split(" ")
+		tweet = all_data["text"] # I am assuming this is a string...
+
+                # Checkpoint 01 - Display the Command
+                print("[Checkpoint 01 ", TIME) # TO BE CHANGED BY CHRISTINA
+
+                # We are inferring that the format is #ECE4564TXX _:____+_____ "..."
+		tag, command = tweet.split(" ", 2)
+		team = tag[-2:] # Grab the last two chars which should be the team number 
+		action, command = command.split(":", 1)
+		place, command = command.split("+", 1)
+		subject, message = command.split(" ", 1)
+
+                # Checkpoint 02 - Save to MongoDB via NoSQL
+                db.____.insertOne(
+                        {
+                               Action: action
+                               Place: place
+                               MsgID: team + "$" + time.time()
+                               Subject: subject
+                               Message: message
+                        }
+                )
+
+                
+                # Checkpoint 03 - Change LED
+                if action == "p":
+                        setLEDR()
+                else if action == "c":
+                        setLEDG()
+                
+		# Sending the message to Repo
+		sendMessageToQueue(self.ip)
+
+                # Set LED back to white to show we are waiting for another command
+                setLEDW()
 		
 
 def sendMessageToQueue(ip):
-	print("ip",ip)
 	connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 	channel = connection.channel()
 
@@ -33,51 +95,24 @@ def sendMessageToQueue(ip):
 	connection.close()
 		
 def main(args):
-
-	"""
-	# setup pins
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(11, GPIO.OUT) # Red
-	GPIO.setup(13, GPIO.OUT) # Green
-	GPIO.setup(15, GPIO.OUT) # Blue
-
-	# white LED - Waiting for command
-	GPIO.output(11, GPIO.HIGH)
-	GPIO.output(13, GPIO.HIGH)
-	GPIO.output(15, GPIO.HIGH)
-	time.sleep(3)
-	"""
 	
 	ip = args.server_ip
 	tag = args.hashtag
-	
-	
-	sendMessageToQueue(ip)
+
+	setLEDW()
 	
 	# setup access to Twitter API
 	auth = OAuthHandler(APIKey, APISecretKey)
 	auth.set_access_token(AccessKey, AccessSecretKey)
 
-	api = tweepy.API(auth)	
+	api = tweepy.API(auth)
+
+	myListener = listener()
+	myListener.setIP(ip)
 	
-	twitterStream = Stream(auth, listener())
+	twitterStream = Stream(auth, myListener)
 	twitterStream.filter(track=[tag])
 	
-	# GET https://api.twitter.com/1.1/search/user_timeline.json?q=tag
-
-	"""
-	# red LED - Received pulish request
-	GPIO.output(13, GPIO.LOW)
-	GPIO.output(15, GPIO.LOW)
-	time.sleep(3)
-
-	# green LED - Received consume request
-	GPIO.output(11, GPIO.LOW)
-	GPIO.output(13, GPIO.HIGH)
-	time.sleep(3)
-	"""
-	
-
 
 if __name__ == "__main__":
 
